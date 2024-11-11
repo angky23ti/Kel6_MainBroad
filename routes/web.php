09589dc2054720de68e5089d52b2874a0dba95b9
angky23ti <angky23ti@mahasiswa.pcr.ac.id>
2024-11-11 11:35:23 +0700

    perubahan ROUTE

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\anggotaController;
+use App\Http\Controllers\anggotaController; //NOW
 
 Route::get('/', function () {
     return view('welcome');
