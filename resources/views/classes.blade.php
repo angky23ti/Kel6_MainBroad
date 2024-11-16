d67360a33dab55f53b5e0711ec6a99af862be939
angky23ti <angky23ti@mahasiswa.pcr.ac.id>
2024-11-16 12:58:23 +0700

    update isi laragon

--- resources/views/classes.blade.php
+++ resources/views/classes.blade.php
@@ -7,10 +7,10 @@
     <title>Data Nama Kelompok 6</title>
 </head>
 <body>
-    <h1>Nama Kelompok:</h1>
+    <h1>Nama Kelompok</h1>
     <h2>1. Angky Kurniawan</h2>
     <h2>2. Anwar Aziz</h2>
     <h2>3. Mohd. Arifin Ramadhan</h2>
-    <h2>4. Sessy Preselia Isabella Simanungkalit</h2>
+    <h2>4. Sessy Preselia</h2>
 </body>
 </html>
